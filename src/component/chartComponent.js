5c5ae64815d9003f46f8b8025dfb1b9cc0dc07c3
HeyImRige <Robert.aron.tx@verizon.net>
2017-04-22 16:54:42 -0500

    randomized graph functioning

--- src/component/chartComponent.js
+++ src/component/chartComponent.js
@@ -1,28 +1,80 @@
-var Chart = require("chart.js").Line;
-var lineChartData = {
-    labels: ["Data 1", "Data 2", "Data 3", "Data 4", "Data 5", "Data 6", "Data 7"],
-    datasets: [{
-        fillColor: "rgba(220,220,220,0)",
-        strokeColor: "rgba(220,180,0,1)",
-        pointColor: "rgba(220,180,0,1)",
-        data: [20, 30, 80, 20, 40, 10, 60]
-    }, {
-        fillColor: "rgba(151,187,205,0)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
-        data: [60, 10, 40, 30, 80, 30, 20]
-    }]
+import React,{Component} from 'react';
+import {Line} from 'react-chartjs-2';
 
-}
-
-var MyComponent = React.createClass({
-    render: function () {
-        
-
-
-
-
-        return 
-            <canvas id="myChart" width="400" height="400"></canvas>
+const initialState = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
     }
+  ]
+};
+
+
+
+const Graph = React.createClass({
+	displayName: 'Graph',
+	componentWillMount(){
+		this.setState(initialState);
+	},
+	componentDidMount(){
+
+		var _this = this;
+
+		setInterval(function(){
+			var oldDataSet = _this.state;
+			var newData = [];
+
+			for(var x=0; x< _this.state.labels.length; x++){
+				newData.push(Math.floor(Math.random() * 100));
+			}
+
+			var newDataSet = {
+				...oldDataSet
+			};
+
+			newDataSet.data = newData;
+
+			_this.setState({datasets: [newDataSet]});
+		}, 5000);
+	},
+	render() {
+		return (
+			<Line data={this.state} />
+		);
+	}
 });
+
+
+
+
+export default class MyGraph extends Component{
+  displayName: 'RandomizedDataLineExample'
+
+  render() {
+    return (
+      <div>
+        <h2>Random Animated Line Example</h2>
+ 		<Graph />
+      </div>
+    );
+  }
+};
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 import './App.css';
+import Chart from '../Component/ChartComponent'
 
 class App extends Component {
 
   render() {
     return(
       <div>
-        <p>Default page that is loaded</p>
-        <a href='./home'>link to the home page</a>
+        <h1>Testing Chart</h1>
+        <Chart></Chart>
       </div>
     )
   };
