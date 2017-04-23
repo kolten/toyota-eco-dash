cdebf4f821bfc56deb6f165c8aa54aa4a49d0001
HeyImRige <Robert.aron.tx@verizon.net>
2017-04-23 03:56:54 -0500

    mostly finished layout

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   render() {
     return(
-      <div>
+      <div className="mainContainer">
         <div className="backgroundIMG"></div>
         <button 
         text =" reset"
